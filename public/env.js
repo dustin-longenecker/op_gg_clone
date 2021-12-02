e76baa5eef139ba3f7d293d11a94dba323f1d7e6
dustin-longenecker <dl-dev@outlook.com>
2021-12-01 18:52:28 -0700

    added api requests, populated matchList state, displayed to screen. need to fix: key props / displaying only 19 of 20 matches

---
+++ public/env.js
@@ -0,0 +1,3 @@
+window.env = {
+  "API_KEY": "api_key=RGAPI-fa8ad744-9d1e-4e68-8791-06abfda85f9e"
+};
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,12 +1,85 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import env from 'react-dotenv'
+
 
 class Landing extends Component {
+  state = {
+    text: '',
+    summoner:'',
+    summonerLevel:'',
+    matchList:[]
+  }
+  
+  submit = (e) => {
+    e.preventDefault()
+    this.setSummoner(this.state.text)
+
+    this.setState(() => ({
+     text : ''
+    }))
+  }
+  change = (e) => {
+    this.setState(() => ({
+      text: e.target.value
+    }))
+  }
+  setSummoner = (text) => {
+    const baseAPIurl = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'
+    let APIkey = "?" + env.API_KEY;
+
+    axios.get(baseAPIurl + text + APIkey).then((res) => {
+      this.setState(() => ({
+        summoner: res.data.name,
+        summonerLevel: res.data.summonerLevel
+      }))
+    this.getMatchList(res.data.puuid)
+
+    }).catch((err) => console.log(err))
+  }
+  getMatchList = (puuid) => {
+    let APIkey = env.API_KEY;
+    const baseAPIurl ='https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/'+ puuid +'/ids?start=0&count=20&'
+    axios.get(baseAPIurl + APIkey).then((res) => { 
+      console.log(res.data)
+      this.transformMatchList(res.data)
+    })
+  }
+  transformMatchList = (array) => {
+    let APIkey = "?" + env.API_KEY;
+    const baseAPIurl = 'https://americas.api.riotgames.com/lol/match/v5/matches/'
+    array.map((item) => {
+      axios.get(baseAPIurl + item + APIkey).then((res) => {
+        this.setState(() => ({
+          matchList: [...this.state.matchList, res.data]
+        }))
+      })
+
+    })
+
+  }
   render() {
- return ( <div>
-   <p>test paragraph</p>
-  </div>
+
+    const {summoner, summonerLevel} = this.state
+   return ( 
+    <div>
+      <form onSubmit={(e) => this.submit(e)}>
+        <input onChange={(e) => this.change(e)}type="text"/>
+        <input type="submit"/>
+      </form>
+      <p>{summoner}</p>
+      <p>{summonerLevel}</p>
+      <ul>
+        {this.state.matchList.map((id) => (
+            <li >
+              {id.metadata.participants}
+            </li>
+          ))}
+      </ul>
+
+    </div>
   )
-}
+ }
 };
 
 export default Landing;
